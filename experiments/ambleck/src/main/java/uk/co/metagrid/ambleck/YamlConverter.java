ad8d9eadaaf9910665c6e5c41eb3c2dfd7a9bf3b
Zarquan <zarquan@metagrid.co.uk>
2024-03-27 07:46:24 +0000

    Fixed copyright message

--- experiments/ambleck/src/main/java/uk/co/metagrid/ambleck/YamlConverter.java
+++ experiments/ambleck/src/main/java/uk/co/metagrid/ambleck/YamlConverter.java
@@ -2,7 +2,7 @@
  *
  * <meta:header>
  *   <meta:licence>
- *     Copyright (C) 2024 by Wizzard Solutions Ltd, wizzard@metagrid.co.uk
+ *     Copyright (C) 2024 University of Manchester.
  *
  *     This information is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
