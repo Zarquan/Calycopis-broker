3e15ebb025614fbdbb79db4c3ae9184c45801191
Dave Morris <dave.morris@manchester.ac.uk>
2026-02-16 19:13:30 +0000

    Added AIMetrics to the changed files and added a rule asking cursor to do it automatically from now on

--- java/src/main/java/net/ivoa/calycopis/datamodel/storage/simple/mock/MockSimpleStorageResourceEntityFactoryImpl.java
+++ java/src/main/java/net/ivoa/calycopis/datamodel/storage/simple/mock/MockSimpleStorageResourceEntityFactoryImpl.java
@@ -1,4 +1,16 @@
-/**
+/*
+ *
+ * AIMetrics: [
+ *     {
+ *     "name": "Cursor CLI",
+ *     "version": "2026.02.13-41ac335",
+ *     "model": "Claude 4.6 Opus (Thinking)",
+ *     "contribution": {
+ *       "value": 100,
+ *       "units": "%"
+ *       }
+ *     }
+ *   ]
  *
  */
 package net.ivoa.calycopis.datamodel.storage.simple.mock;
